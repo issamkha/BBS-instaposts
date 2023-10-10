784a71c231038cd334f2e032dda06aa235323d28
ikdev <ikhairi@ikdev.fr>
2023-10-10 19:09:30 +0200

    Adding translation en/fr

---
+++ lang/fr/auth.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'incorrect_password' => 'Le mot de passe fourni est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+    'email' => 'Email',
+    'password' => 'Mot de passe',
+    'remember_me' => 'Se souvenir de moi',
+    'forgot_password' => 'Mot de passe oublié ?',
+    'log_in' => 'Se connecter',
+
+];
