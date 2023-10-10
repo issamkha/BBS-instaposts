9078c2071dbbf1ba7dc6b812afe715eae9379036
ikdev <ikhairi@ikdev.fr>
2023-10-10 19:17:47 +0200

    Creation of the 'instagram_api_link' table and model

---
+++ app/Models/InstagramApiLink.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InstagramApiLink extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'url',
+        'user_id',
+        'endpoint',
+        'fields',
+        'access_token'
+    ];
+}
