902bc0d61901980351939daba1b205e4f8f656f6
ikdev <ikhairi@ikdev.fr>
2023-10-10 19:01:07 +0200

    Setting up Bootstrap and the BBS logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,14 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Scripts App, Bootstrap -->
+        @vite([
+            'resources/css/app.css',
+            'resources/css/bootstrap.min.css',
+            'resources/js/bootstrap.bundle.min.js',
+            'resources/js/bootstrap.min.js',
+            'resources/js/app.js'
+        ])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
